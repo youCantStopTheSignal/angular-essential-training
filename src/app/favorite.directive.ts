9a4455dce800c0b836145cb376951d8963b15a7f
youCantStopTheSignal <tonyhansen_public@outlook.com>
2021-11-11 15:10:21 -0600

    lesson changes - events

--- src/app/favorite.directive.ts
+++ src/app/favorite.directive.ts
@@ -1,11 +1,18 @@
-import { Directive, HostBinding, Input } from '@angular/core';
+import { Directive, HostBinding, HostListener, Input } from '@angular/core';
 
 @Directive({
-  selector: '[mwFavorite]'
+	selector: '[mwFavorite]'
 })
 export class FavoriteDirective {
-  @HostBinding('class.is-favorite') isFavorite = true;
-  @Input() set mwFavorite(value) {
-    this.isFavorite = value;
-  }
+	@HostBinding('class.is-favorite') isFavorite = true;
+	@HostBinding('class.is-favorite-hovering') hovering = false;
+	@HostListener('mouseenter') onMouseEnter() {
+		this.hovering = true;
+	}
+	@HostListener('mouseleave') onMouseLeave() {
+		this.hovering = false;
+	}
+	@Input() set mwFavorite(value) {
+		this.isFavorite = value;
+	}
 }
