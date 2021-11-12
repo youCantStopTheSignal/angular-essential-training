61d1dc979a4c0521f4f6783afd890a2369476e26
youCantStopTheSignal <tonyhansen_public@outlook.com>
2021-11-12 14:34:41 -0600

    form-style form validation

--- src/app/media-item-form.component.ts
+++ src/app/media-item-form.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
-  selector: 'mw-media-item-form',
-  templateUrl: './media-item-form.component.html',
-  styleUrls: ['./media-item-form.component.css']
+	selector: 'mw-media-item-form',
+	templateUrl: './media-item-form.component.html',
+	styleUrls: ['./media-item-form.component.css']
 })
-export class MediaItemFormComponent {
-  onSubmit(mediaItem) {
-    console.log(mediaItem);
-  }
+
+export class MediaItemFormComponent implements OnInit {
+	form: FormGroup;
+	
+	ngOnInit() {
+		this.form = new FormGroup({
+			medium: new FormControl('Movies'),
+			name:  new FormControl(''),
+			category:  new FormControl(''),
+			year:  new FormControl(''),
+		});
+	}
+	
+	onSubmit(mediaItem) {
+		console.log(mediaItem);
+	}
 }
