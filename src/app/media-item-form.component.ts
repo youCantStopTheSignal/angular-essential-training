735d15d2182f025b349a62847e5c4a2f038e6641
youCantStopTheSignal <tonyhansen_public@outlook.com>
2021-11-13 13:06:17 -0600

    custom form validation

--- src/app/media-item-form.component.ts
+++ src/app/media-item-form.component.ts
@@ -17,10 +17,30 @@ export class MediaItemFormComponent implements OnInit {
         Validators.pattern('[\\w\\-\\s\\/]+')
       ])),
       category: new FormControl(''),
-      year: new FormControl(''),
+      year: new FormControl('', this.yearValidator),
     });
   }
 
+  yearValidator(control: FormControl) {
+    // not a required field
+    if (control.value.trim().length === 0) {
+      console.log('no year value present');
+      return null;
+    }
+
+    const year = parseInt(control.value, 10);
+    const minYear = 1900;
+    const maxYear = 2100;
+
+    if (year >= minYear && year <= maxYear) {
+      // year is valid
+      console.log(year +' is between ' + minYear + " and " + maxYear);
+      return null;
+    } else {
+      console.log(year +' is NOT between ' + minYear + " and " + maxYear);
+      return {year: true};
+    }
+  }
   onSubmit(mediaItem) {
     console.log(mediaItem);
   }
